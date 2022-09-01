cc9a9cb2a741380b1f597b428618dc859af865a8
unknown <kjh030529@gmail.com>
2022-09-01 09:56:05 +0900

    Commit Test1 Solution

---
+++ Test1/1/1.cpp
@@ -0,0 +1,12 @@
+﻿// 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <stdio.h>
+
+int main()
+{
+	float c = 0;
+	scanf_s("%f", &c);
+
+	printf("f : %f", 1.8 * c + 32);
+}
