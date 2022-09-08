b35fe309b4c12e440b5c81e9819faff022a80bb0
KIBA <kjh030529@gmail.com>
2022-09-08 10:46:13 +0900

    Add Test11 solution

---
+++ Test11/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	scanf("%d");
+	switch(a)
+	{
+		case 1:
+			printf("winter");
+			break;
+		case 2:
+			printf("winter");
+			break;
+		case 3:
+			printf("spring");
+			break;
+		default:
+			printf("Wrong");
+			break;
+	}
+	//printf("winter");
+}
+
--- Test9/main.c
+++ Test9/main.c
@@ -12,5 +12,21 @@ int main()
 	printf("\nOver : %d", BMI >= 25.0);
 	printf("\nFat : %d", 30.0 <= BMI);
 
+
+	printf("\n\n당신의 체질량 지수는 '");
+	if(BMI >= 30)
+	{
+		printf("비만");
+	}
+	else if(BMI >= 25)
+	{
+		printf("과체중");
+	}
+	else
+	{
+		printf("정상");
+	}
+
+	printf("'범위 내에 있습니다.");
 	return 0;
 }
