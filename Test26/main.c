eeb674df43dccfef29ead293222d9ef948ea51ba
KIBA <kjh030529@gmail.com>
2022-10-06 09:52:16 +0900

    Commit Test26 : 2D Array3

---
+++ Test26/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int a[2][3] = {1, 2, 3, 4, 5, 6};
+
+	int i, j;
+
+	for(i = 0 ; i< 2; i++)
+	{
+		for(j = 2; j>= 0; --j)
+		{
+			printf("%d ", a[i][j]);
+		}
+		printf("\n");
+	}
+}
