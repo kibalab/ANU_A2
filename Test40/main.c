c6346d14669a151edea918f1abc94c80f629ff9b
KIBA <kjh030529@gmail.com>
2022-10-13 21:39:12 +0900

    Commit Test40 : print name with star line

---
+++ Test40/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void star();
+void hello();
+
+int main(void)
+{
+	star();
+	hello();
+	star();
+}
+
+void star()
+{
+	printf("*****");
+}
+
+void hello()
+{
+	printf(" Hello C World ");
+}
+
